914cbf3c6a3bd6ffdab2de4a3e9392ddfe35f325
junket <jonathan@junketdesign.com>
2013-12-13 16:53:01 -0500

    Skip precompile

--- config/application.rb
+++ config/application.rb
@@ -67,6 +67,8 @@ module ForbesHackathon
     
     # Precompile some things
     config.assets.precompile += ['home.css', 'home.js']
+    
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'

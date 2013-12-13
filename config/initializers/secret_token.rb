072b5813490dca3836e908610b563c200373dc88
junket <jonathan@junketdesign.com>
2013-12-12 20:08:03 -0500

    Initial commit Rails 3.2.11

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForbesHackathon::Application.config.secret_token = '3a1838f256675f2f01811175bd4d5009c6e94d40b05159f1f440d29a2f2daeb7fd306070ebab7d5e2196c6398701081e580c6785a4685f2d4f2a2d4b86420af3'
